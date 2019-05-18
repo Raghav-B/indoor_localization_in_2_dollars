40c918148cf3bd9495b2ede83a698fb6753e2d49
raghav-b <raghavbhardwaj@u.nus.edu>
2019-05-19 04:33:39 +0800

    Ready to test dfs/fake sssp

--- Assets/Scripts/PersonLocator.cs
+++ Assets/Scripts/PersonLocator.cs
@@ -15,20 +15,37 @@ public class PersonLocator : MonoBehaviour
     public float init_pos_z = 0f;
     public float movement_speed = 2f;
 
+    //public GameObject[] node_col = new GameObject[35];
+
     GameObject cur_person;
+
+    public GameObject mcu1;
+    public GameObject mcu2;
+    public GameObject mcu3;
+
+    public GameObject mcu1_temp_fire;
+    public GameObject mcu2_temp_fire;
+    public GameObject mcu3_temp_fire;
+
+    public Material green_node_mat;
+    public Material red_node_mat;
 
     // Start is called before the first frame update
     Thread udp_thread;
+    Thread temp_sensor_thread;
 
 
     string prev_output = "";
     string output = "";
 
-    float new_pos_x = 0;
-    float new_pos_z = 0;
+    float new_pos_x = 9;
+    float new_pos_z = 6;
 
-    void Start()
-    {
+    public bool mcu1_off = false;
+    public bool mcu2_off = false;
+    public bool mcu3_off = false;
+
+    void Start() {
         cur_person = GameObject.Instantiate(person_template);
         cur_person.transform.SetPositionAndRotation(new Vector3(init_pos_x, init_pos_y, init_pos_z), cur_person.transform.rotation);
 
@@ -36,9 +53,69 @@ public class PersonLocator : MonoBehaviour
         udp_thread.SetApartmentState(ApartmentState.STA);
         udp_thread.Start();
 
-        new_pos_x = cur_person.transform.position.x;
-        new_pos_z = cur_person.transform.position.z;
+        temp_sensor_thread = new Thread(new ThreadStart(start_temp_sensors));
+        temp_sensor_thread.SetApartmentState(ApartmentState.STA);
+        temp_sensor_thread.Start();
+
+        //new_pos_x = cur_person.transform.position.x;
+        //new_pos_z = cur_person.transform.position.z;
         //GameObject.Instantiate(person_template, new Vector3(init_pos_x, init_pos_y, init_pos_z);
+
+        mcu1_temp_fire.SetActive(false);
+        mcu2_temp_fire.SetActive(false);
+        mcu3_temp_fire.SetActive(false);
+    }
+
+    void start_temp_sensors() {
+        while (true) {
+            UdpClient mcu1_listener;
+            UdpClient mcu2_listener;
+            UdpClient mcu3_listener;
+            IPEndPoint mcu1_end_point;
+            IPEndPoint mcu2_end_point;
+            IPEndPoint mcu3_end_point;
+            byte[] mcu1_buffer;
+            byte[] mcu2_buffer;
+            byte[] mcu3_buffer;
+            //Data variable
+
+            string mcu1_ip = "127.0.4.1";
+            string mcu2_ip = "127.0.2.1";
+            string mcu3_ip = "127.0.3.1";
+
+            bool ip_parse_success = IPAddress.TryParse(mcu1_ip, out IPAddress parsed_mcu1_ip);
+            ip_parse_success = IPAddress.TryParse(mcu2_ip, out IPAddress parsed_mcu2_ip);
+            ip_parse_success = IPAddress.TryParse(mcu3_ip, out IPAddress parsed_mcu3_ip);
+
+            mcu1_end_point = new IPEndPoint(parsed_mcu1_ip, 2102);
+            mcu1_listener = new UdpClient(2102);
+            mcu2_end_point = new IPEndPoint(parsed_mcu2_ip, 2103);
+            mcu2_listener = new UdpClient(2102);
+            mcu3_end_point = new IPEndPoint(parsed_mcu3_ip, 2104);
+            mcu3_listener = new UdpClient(2102);
+
+            mcu1_buffer = mcu1_listener.Receive(ref mcu1_end_point);
+            mcu2_buffer = mcu2_listener.Receive(ref mcu2_end_point);
+            mcu3_buffer = mcu3_listener.Receive(ref mcu3_end_point);
+
+            string mcu1_output = Encoding.ASCII.GetString(mcu1_buffer, 0, mcu1_buffer.Length);
+            string mcu2_output = Encoding.ASCII.GetString(mcu2_buffer, 0, mcu2_buffer.Length);
+            string mcu3_output = Encoding.ASCII.GetString(mcu3_buffer, 0, mcu3_buffer.Length);
+
+            if (Int32.Parse(mcu1_output) < 100) {
+                mcu1_off = true;
+            }
+            if (Int32.Parse(mcu2_output) < 100) {
+                mcu2_off = true;
+            }
+            if (Int32.Parse(mcu3_output) < 100) {
+                mcu3_off = true;
+            }
+
+            mcu1_listener.Close();
+            mcu2_listener.Close();
+            mcu3_listener.Close();
+        }
     }
 
     void start_udp() {
@@ -70,15 +147,102 @@ public class PersonLocator : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (Input.GetKeyUp(KeyCode.Return)) {
             udp_thread.Abort();
+            temp_sensor_thread.Abort();
+
         } else if (Input.GetKeyUp(KeyCode.RightShift)) {
             udp_thread.Start();
+            temp_sensor_thread.Start();
         }
 
-        cur_person.transform.position = Vector3.MoveTowards(cur_person.transform.position, new Vector3(new_pos_x, 0, new_pos_z), 0.2f);
+        int unity_pos_x = 0;
+        int unity_pos_z = 0;
 
+        switch (new_pos_x) {
+            case 0:
+                unity_pos_x = 18; break;
+            case 3:
+                unity_pos_x = 13; break;
+            case 6:
+                unity_pos_x = 6; break;
+            case 9:
+                unity_pos_x = 0; break;
+            case 12:
+                unity_pos_x = -6; break;
+            case 15:
+                unity_pos_x = -18; break;
+        }
+
+        switch (new_pos_z) {
+            case 0:
+                unity_pos_z = 11; break;
+            case 3:
+                unity_pos_z = 6; break;
+            case 6:
+                unity_pos_z = 0; break;
+            case 9:
+                unity_pos_z = -6; break;
+            case 12:
+                unity_pos_z = 11; break;
+        }
+
+        cur_person.transform.position = Vector3.MoveTowards(cur_person.transform.position, new Vector3(unity_pos_x, 0, unity_pos_z), 0.2f);
+
+        
+        if (mcu1_off) {
+            mcu1_temp_fire.SetActive(true);
+            Collider[] hit_colliders = Physics.OverlapSphere(mcu1_temp_fire.transform.position, 15);
+            foreach (var x in hit_colliders) {
+                if (x.tag == "Node") {
+                    x.transform.GetComponent<MeshRenderer>().material = red_node_mat;
+                }
+            }
+        } else {
+            Collider[] hit_colliders = Physics.OverlapSphere(mcu1_temp_fire.transform.position, 15);
+            foreach (var x in hit_colliders) {
+                if (x.tag == "Node") {
+                    x.transform.GetComponent<MeshRenderer>().material = green_node_mat;
+                }
+            }
+            mcu1_temp_fire.SetActive(false);
+        }
+
+        if (mcu2_off) {
+            mcu2_temp_fire.SetActive(true);
+            Collider[] hit_colliders = Physics.OverlapSphere(mcu2_temp_fire.transform.position, 15);
+            foreach (var x in hit_colliders) {
+                if (x.tag == "Node") {
+                    x.transform.GetComponent<MeshRenderer>().material = red_node_mat;
+                }
+            }
+        } else {
+            Collider[] hit_colliders = Physics.OverlapSphere(mcu2_temp_fire.transform.position, 15);
+            foreach (var x in hit_colliders) {
+                if (x.tag == "Node") {
+                    x.transform.GetComponent<MeshRenderer>().material = green_node_mat;
+                }
+            }
+            mcu2_temp_fire.SetActive(false);
+        }
+
+        if (mcu3_off) {
+            mcu3_temp_fire.SetActive(true);
+            Collider[] hit_colliders = Physics.OverlapSphere(mcu3_temp_fire.transform.position, 15);
+            foreach (var x in hit_colliders) {
+                if (x.tag == "Node") {
+                    x.transform.GetComponent<MeshRenderer>().material = red_node_mat;
+                }
+            }
+        } else {
+            Collider[] hit_colliders = Physics.OverlapSphere(mcu3_temp_fire.transform.position, 15);
+            foreach (var x in hit_colliders) {
+                if (x.tag == "Node") {
+                    x.transform.GetComponent<MeshRenderer>().material = green_node_mat;
+                }
+            }
+            mcu3_temp_fire.SetActive(false);
+        }
     }
 }
---
+++ Assets/Scripts/ShortestPath.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ShortestPath : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData click) {
+        if (click.button == PointerEventData.InputButton.Left) {
+            start_fake_sssp();
+        }
+    }
+
+    public GameObject[] nodes_col = new GameObject[35];
+    public GameObject start_node;
+    public int start_node_index = 34;
+    public GameObject end_node;
+    public int end_node_index;
+    GameObject cur_node;
+    public int cur_node_index;
+
+    public Material red_node_mat;
+    public Material green_node_mat;
+
+    List<int> path_indexes = new List<int>;
+
+    void dfs(int append_node) {
+        //bool path_found = false;
+        if (cur_node_index - 7 >= 0) {
+            if (nodes_col[cur_node_index - 7].transform.GetComponent<MeshRenderer>().material == green_node_mat) { //Up
+                cur_node = nodes_col[cur_node_index - 7];
+                cur_node_index -= 7;
+                //path_found = true;
+                dfs(cur_node_index);
+            }
+        }
+
+        if ((cur_node_index - 1) % 7 == 0) {
+            if (nodes_col[cur_node_index - 1].transform.GetComponent<MeshRenderer>().material == green_node_mat) { //Right
+                cur_node = nodes_col[cur_node_index - 1];
+                cur_node_index -= 1;
+                //path_found = true;
+                dfs(cur_node_index);
+            }
+        }
+
+        if (cur_node_index + 7 <= 28) {
+            if (nodes_col[cur_node_index + 7].transform.GetComponent<MeshRenderer>().material == green_node_mat) { //Down
+                cur_node = nodes_col[cur_node_index + 7];
+                cur_node_index += 7;
+                //path_found = true;
+                dfs(cur_node_index);
+            }
+        }
+
+        if ((cur_node_index + 1) % 7 >= 0) {
+            if (nodes_col[cur_node_index + 1].transform.GetComponent<MeshRenderer>().material == green_node_mat) { //Left
+                cur_node = nodes_col[cur_node_index + 7];
+                cur_node_index += 1;
+                //path_found = true;
+                dfs(cur_node_index);
+            }
+        }
+
+        if (append_node == end_node_index) {
+            path_indexes.Add(end_node_index);
+            end_node_index = append_node;
+            return;
+        } else {
+            return;
+        }
+    }
+
+    void start_fake_sssp() {
+        cur_node = start_node;
+        cur_node_index = start_node_index;
+        dfs(cur_node_index);
+
+        foreach (var x in path_indexes) {
+            Debug.Log(x.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
